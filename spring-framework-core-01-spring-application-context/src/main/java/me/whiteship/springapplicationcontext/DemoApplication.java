0fa79355b5c36bd6197a368d1a59e9ae8872b3cc
sojeongw <redthink90@gmail.com>
2020-03-12 21:23:16 +0900

    스프링 프레임워크 핵심 기술: ApplicationContext - @SpringBootApplication

--- spring-framework-core-01-spring-application-context/src/main/java/me/whiteship/springapplicationcontext/DemoApplication.java
+++ spring-framework-core-01-spring-application-context/src/main/java/me/whiteship/springapplicationcontext/DemoApplication.java
@@ -1,26 +1,10 @@
 package me.whiteship.springapplicationcontext;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        // 자바 설정 파일을 이용해 불러온다.
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-
-        // [bookService, bookRepository]
-        System.out.println(Arrays.toString(beanDefinitionNames));
-
-        // 출력된 빈 이름으로 불러온다.
-        BookService bookService = (BookService)context.getBean("bookService");
-        // true
-        System.out.println(bookService.bookRepository != null);
     }
 }
