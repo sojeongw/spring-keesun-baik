15c1618a2ca1206fca980bc0727947ef53fcd269
sojeongw <redthink90@gmail.com>
2020-03-12 15:14:00 +0900

    스프링 프레임워크 핵심 기술: ApplicationContext - 컴포넌트 스캔

--- spring-framework-core-01-spring-application-context/src/main/java/me/whiteship/springapplicationcontext/BookService.java
+++ spring-framework-core-01-spring-application-context/src/main/java/me/whiteship/springapplicationcontext/BookService.java
@@ -1,9 +1,16 @@
 package me.whiteship.springapplicationcontext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+    @Autowired
     BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 }
+
+
