4ac94fb6ed7ca3795c1427458f540d2ee92ec4eb
sojeongw <redthink90@gmail.com>
2020-03-15 06:11:37 +0900

    스프링 프레임워크 핵심 기술: 빈 이름 지정

--- spring-framework-core-02-autowired/src/main/java/me/whiteship/autowired/BookService.java
+++ spring-framework-core-02-autowired/src/main/java/me/whiteship/autowired/BookService.java
@@ -3,14 +3,12 @@ package me.whiteship.autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class BookService {
     @Autowired
-    List<BookRepository> bookRepositories;
+    BookRepository myBookRepository;
 
     public void printBookRepository() {
-        this.bookRepositories.forEach(System.out::println);
+        System.out.println(myBookRepository.getClass());
     }
 }
