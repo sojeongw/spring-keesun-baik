905a72e0240274210a0cb10794b057a2abd108eb
sojeongw <redthink90@gmail.com>
2020-03-14 19:23:19 +0900

    스프링 프레임워크 핵심 기술: 여러 개의 빈 주입

--- spring-framework-core-02-autowired/src/main/java/me/whiteship/autowired/BookService.java
+++ spring-framework-core-02-autowired/src/main/java/me/whiteship/autowired/BookService.java
@@ -1,16 +1,16 @@
 package me.whiteship.autowired;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
     @Autowired
-    @Qualifier("keesunBookRepository")
-    BookRepository bookRepository;
+    List<BookRepository> bookRepositories;
 
     public void printBookRepository() {
-        System.out.println(bookRepository.getClass());
+        this.bookRepositories.forEach(System.out::println);
     }
 }
