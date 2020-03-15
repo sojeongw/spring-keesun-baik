c111f7757c251b6fb613a6a8436ddd4dfe66174d
sojeongw <redthink90@gmail.com>
2020-03-15 10:04:03 +0900

    스프링 프레임워크 핵심 기술: AutowiredAnnotationBeanPostProcessor

--- spring-framework-core-02-autowired/src/main/java/me/whiteship/autowired/BookService.java
+++ spring-framework-core-02-autowired/src/main/java/me/whiteship/autowired/BookService.java
@@ -3,12 +3,16 @@ package me.whiteship.autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public class BookService {
     @Autowired
     BookRepository myBookRepository;
 
-    public void printBookRepository() {
+    // 이제 Runner는 필요없다.
+    @PostConstruct
+    public void setup() {
         System.out.println(myBookRepository.getClass());
     }
 }
