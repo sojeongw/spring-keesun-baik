964ea4c78fec19ba066ad1156ba1a585460eb1c0
sojeongw <redthink90@gmail.com>
2020-03-16 23:58:15 +0900

    스프링 프레임워크 핵심 기술: Environment - @Profile로 profile 등록

--- spring-framework-core-05-profile/src/main/java/me/whiteship/profile/TestConfiguration.java
+++ spring-framework-core-05-profile/src/main/java/me/whiteship/profile/TestConfiguration.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("test")
-public class TestConfiguration {
-    @Bean
-    public BookRepository bookRepository() {
-        return new TestBookRepository();
-    }
-}
+//@Configuration
+//@Profile("test")
+//public class TestConfiguration {
+//    @Bean
+//    public BookRepository bookRepository() {
+//        return new TestBookRepository();
+//    }
+//}
