e17bad9bcd58f21bde9c80c2f35cef9c36e4277e
sojeongw <redthink90@gmail.com>
2020-03-16 22:08:54 +0900

    스프링 프레임워크 핵심 기술: 빈 스코프 - 싱글턴과 프로토타입

--- spring-framework-core-04-scope/src/main/java/me/whiteship/beanscope/Proto.java
+++ spring-framework-core-04-scope/src/main/java/me/whiteship/beanscope/Proto.java
@@ -1,7 +1,9 @@
 package me.whiteship.beanscope;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Proto {
 }
