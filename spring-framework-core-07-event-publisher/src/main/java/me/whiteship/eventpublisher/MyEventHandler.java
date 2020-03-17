088bebd501e4deabb7d17c0a336d4486e4bc2cf9
sojeongw <redthink90@gmail.com>
2020-03-17 17:20:29 +0900

    스프링 프레임워크 핵심 기술: ApplicationEventPublisher - 스프링 기본 이벤트

--- spring-framework-core-07-event-publisher/src/main/java/me/whiteship/eventpublisher/MyEventHandler.java
+++ spring-framework-core-07-event-publisher/src/main/java/me/whiteship/eventpublisher/MyEventHandler.java
@@ -1,5 +1,7 @@
 package me.whiteship.eventpublisher;
 
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -11,5 +13,19 @@ public class MyEventHandler{
     public void handle(MyEvent myEvent) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트 받았다. 데이터는 " + myEvent.getData());
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextRefreshedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextRefreshedEvent");
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextClosedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextClosedEvent");
     }
 }
