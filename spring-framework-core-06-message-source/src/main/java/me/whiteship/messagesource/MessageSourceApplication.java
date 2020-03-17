b97dc7671a13718a89ffe9f7ebb760e02445c6bd
sojeongw <redthink90@gmail.com>
2020-03-17 14:56:39 +0900

    스프링 프레임워크 핵심 기술: MessageSource - 직접 빈 등록

--- spring-framework-core-06-message-source/src/main/java/me/whiteship/messagesource/MessageSourceApplication.java
+++ spring-framework-core-06-message-source/src/main/java/me/whiteship/messagesource/MessageSourceApplication.java
@@ -2,6 +2,9 @@ package me.whiteship.messagesource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class MessageSourceApplication {
@@ -10,4 +13,14 @@ public class MessageSourceApplication {
         SpringApplication.run(MessageSourceApplication.class, args);
     }
 
+    // message를 직접 정의하는 방법
+    @Bean
+    // 빈 이름은 항상 messageSource가 되어야 한다.
+    public MessageSource messageSource() {
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        // 한글이 깨지므로 인코딩 해준다.
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
