eb1640f8bb3fb699a3225647defe325262a79976
sojeongw <redthink90@gmail.com>
2020-03-19 06:23:47 +0900

    스프링 프레임워크 핵심 기술: Resource - ApplicationContext에 따라 리소스 읽기

--- spring-framework-core-09-resource-abstraction/src/main/java/me/whiteship/resourceabstraction/AppRunner.java
+++ spring-framework-core-09-resource-abstraction/src/main/java/me/whiteship/resourceabstraction/AppRunner.java
@@ -3,9 +3,12 @@ package me.whiteship.resourceabstraction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,9 +20,12 @@ public class AppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Resource resource = resourceLoader.getResource("classpath:test.txt");
-        System.out.println(resource.exists());
-        System.out.println(resource.getDescription());
-        System.out.println(Files.readString(Path.of(resource.getURI())));
+        // 클래스 패스 기준으로 configLocation에 해당하는 문자열을 가지고 빈 설정 파일을 찾는다.
+        var ctx1 = new ClassPathXmlApplicationContext("configtest.xml");
+        // 파일 시스템 기준으로 configLocation에 해당하는 문자열을 가지고 빈 설정 파일을 찾는다.
+        var ctx2 = new FileSystemXmlApplicationContext("configtest.xml");
+        // 애플리케이션 루트 기준으로 configLocation에 해당하는 문자열을 가지고 빈 설정 파일을 찾는다.
+        var ctx3 = new WebApplicationContext("config.xml");
+
     }
 }
