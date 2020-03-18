394d814ed783b4fabb8b3d1cea6dceed9a4fe73e
sojeongw <redthink90@gmail.com>
2020-03-19 05:49:47 +0900

    스프링 프레임워크 핵심 기술: ResourceLoader

---
+++ spring-framework-core-09-resource-abstraction/src/main/java/me/whiteship/resourceabstraction/ResourceAbstractionApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship.resourceabstraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAbstractionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceAbstractionApplication.class, args);
+    }
+
+}
