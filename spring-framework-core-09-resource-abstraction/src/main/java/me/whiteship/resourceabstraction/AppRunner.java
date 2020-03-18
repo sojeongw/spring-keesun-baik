072925fc31100ad8a458b8f4b0bcc7bd110672bb
sojeongw <redthink90@gmail.com>
2020-03-19 06:38:34 +0900

    스프링 프레임워크 핵심 기술: Resource - classpath 없이 리소스 읽어오기

--- spring-framework-core-09-resource-abstraction/src/main/java/me/whiteship/resourceabstraction/AppRunner.java
+++ spring-framework-core-09-resource-abstraction/src/main/java/me/whiteship/resourceabstraction/AppRunner.java
@@ -23,10 +23,13 @@ public class AppRunner implements ApplicationRunner {
         // 리소스 로더의 타입. 웹 애플리케이션 리소스로 나와야 한다.
         System.out.println(resourceLoader.getClass());
 
-        Resource resource = resourceLoader.getResource("classpath:test.txt");
-        // 리소스의 타입. 클래스 패스 리소스가 나와야 한다.
+        // classpath를 뺐을 때는 리소스 로더가 웹 애플리케이션 컨텍스트이므로 서블릿으로 나와야 한다.
+        Resource resource = resourceLoader.getResource("test.txt");
         System.out.println(resource.getClass());
 
+        // 따라서 이 리소스를 웹 애플리케이션 루트부터 찾게 된다.
+        // 하지만 스프링 부트에서 톰캣은 컨텍스트 패스가 기본적으로 지정되어있지 않다.
+        // 결국 리소스를 찾을 수 없다.
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
         System.out.println(Files.readString(Path.of(resource.getURI())));
